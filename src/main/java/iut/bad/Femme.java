15e2bba707d656596e442368de1c2f9e481f32be
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-03-29 00:12:15 +0300

    classe Femmme

---
+++ src/main/java/iut/bad/Femme.java
@@ -0,0 +1,16 @@
+package iut.bad;
+
+public class Femme {
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Femme() {
+    }
+
+    public Femme(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+}
