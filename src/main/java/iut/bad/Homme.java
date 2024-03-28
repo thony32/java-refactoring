33865e9eea2c4a3eeb1a39e6b069d179b5804ae1
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-03-29 00:11:21 +0300

    classe Homme

---
+++ src/main/java/iut/bad/Homme.java
@@ -0,0 +1,16 @@
+package iut.bad;
+
+public class Homme {
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Homme() {
+    }
+
+    public Homme(int age, String nom, String prenom) {
+        this.age = age;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
---
+++ src/main/java/iut/bad/Main.java
@@ -0,0 +1,7 @@
+package iut.bad;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
